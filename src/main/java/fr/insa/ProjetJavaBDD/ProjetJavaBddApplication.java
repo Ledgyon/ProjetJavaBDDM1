5ede0154bac1ffd10fcb24ea0b32e89ef8469d84
Simon Rudent <Ledgyon>
2021-11-27 09:58:53 +0100

    first commit

---
+++ src/main/java/fr/insa/ProjetJavaBDD/ProjetJavaBddApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.ProjetJavaBDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJavaBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJavaBddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/insa/ProjetJavaBDD/ProjetJavaBddApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.ProjetJavaBDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJavaBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
